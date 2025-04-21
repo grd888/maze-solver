6cfa5ef49cd4f6904092821f66c82c9727b3a43a
Greg Delgado <grd888@gmail.com>
2025-04-22 07:06:30 +0800

    add function to reset visited cells to false; also add unit test

--- maze.py
+++ maze.py
@@ -28,6 +28,7 @@ class Maze:
     self._create_cells()
     self._break_entrance_and_exit()
     self._break_walls_r(0, 0)
+    self._reset_cells_visited()
     
   def _create_cells(self):
     # Initialize the cells matrix
@@ -121,3 +122,8 @@ class Maze:
       
       # Recursively visit the chosen cell
       self._break_walls_r(next_i, next_j)
+      
+  def _reset_cells_visited(self):
+    for col in self._cells:
+      for cell in col:
+        cell.visited = False
